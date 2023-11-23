dd6db2088d78159b96c7ccce1327efc19f63237c
Deniz <77787415+deniz244@users.noreply.github.com>
2023-11-23 16:04:42 +0300

    vampire class is not abstract no more, poison method override

--- src/com/workintech/rpgGame/monsters/Vampire.java
+++ src/com/workintech/rpgGame/monsters/Vampire.java
@@ -2,7 +2,7 @@ package com.workintech.rpgGame.monsters;
 
 import com.workintech.rpgGame.Monster;
 
-public abstract class Vampire extends Monster {
+public class Vampire extends Monster {
 
     public Vampire(String name, int hitPoints, double damage) {
         super(name, hitPoints, damage);
@@ -13,4 +13,8 @@ public abstract class Vampire extends Monster {
         return getDamage()*0.25;
     }
 
+    @Override
+    public double poison() {
+        return getDamage()*0.3;
+    }
 }
